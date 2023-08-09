cfb38ff9815ad33974656f3423fd78f58fc05833
MrMurder4all <64412699+MrMurder4all@users.noreply.github.com>
2023-08-09 18:48:06 +0300

    Добавьте файлы проекта.

---
+++ 10to16.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+#include <string>
+#include <sstream>
+#include <iomanip>
+#include <vector>
+
+using namespace std;
+
+string decimalToHexadecimal(int decimalValue) {
+    stringstream ss;
+    ss << hex << decimalValue;
+    return ss.str();
+}
+
+int main() {
+    string input;
+    getline(cin, input);
+
+    vector<int> massiv;
+    istringstream iss(input);
+    stringstream result_ss;
+
+    while (iss.good()) {
+        string substr;
+        getline(iss, substr, ',');
+
+        string value = decimalToHexadecimal(stoi(substr));
+        if (stoi(substr) < 16) {
+            value = "0" + value;
+        };
+        result_ss << value;
+        
+    }
+    string result = result_ss.str();
+    cout << result << endl;
+
+
+    return 0;
+}
